09022c2fecb285fa37bf3c418cc8a2b458218e0b
Mansi Mittal <mansimittal1426@gmail.com>
2026-03-23 22:29:53 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (80.23%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,8 +4,10 @@ public:
         int size = nums.size();
         int maxSum=nums[0], currentMaxSum=nums[0];
         for(int i=1; i<size; i++){
-            maxSum = max(nums[i], maxSum + nums[i]);
-            currentMaxSum = max(currentMaxSum, maxSum);   
+            if(nums[i] <= 10000 && nums[i] >= -10000){
+                maxSum = max(nums[i], maxSum + nums[i]);
+                currentMaxSum = max(currentMaxSum, maxSum);
+            }
         }
         return currentMaxSum;
     }
