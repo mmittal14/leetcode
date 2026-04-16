964597d3e52e8229cf18fef0e1c7e60b7604c64d
Mansi Mittal <mansimittal1426@gmail.com>
2026-04-16 11:47:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (69.88%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int left=0,right=0;
-        int maxLen=0,maxFreq=0;
-        map<int,int> mp;
-        int n=s.size();
-        while(right<n){
-            mp[s[right]-'A']++;
-            maxFreq=max(maxFreq,mp[s[right]-'A']);
-            if((right-left+1-maxFreq)>k){
-                mp[s[left]-'A']--;
-                left++;
-            }
-            if((right-left+1-maxFreq)<=k){
-                maxLen=max(maxLen,right-left+1);
-            }
-            right++;
+    vector<int> freq(26, 0);
+    
+    int l = 0, maxFreq = 0, maxLen = 0;
+
+    for (int r = 0; r < s.size(); r++) {
+        freq[s[r] - 'A']++;
+        maxFreq = max(maxFreq, freq[s[r] - 'A']);
+
+        int windowSize = r - l + 1;
+        if (windowSize - maxFreq > k) {
+            freq[s[l] - 'A']--;
+            l++;
         }
-        return maxLen;
+
+        maxLen = max(maxLen, r - l + 1);
+    }
+
+    return maxLen;
     }
 };
